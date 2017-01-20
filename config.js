7cce09d1cfb076cfa6bcb75f16be84fda67235cf
benwingz <benjamin.roullet@gmail.com>
2017-01-20 16:55:54 +0100

    :trollface: Get exemple of config js file

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  'secret': '', //YOUR SECRET TO ENCODE JWT
+  'database': '', //LINK TO YOUR MONGODB
+  'showtimeProviderAPIkey' : '', //PARTNER CODE TO YOUR SHOWTIME PROVIDER
+  'showtimeProviderUrl' : '', //URL TO YOUR SHOWTIME PROVIDER API
+  'apiKeyMovieDB' : '', //YOUR API KEY OF MOVIEDB
+  'movieApiBaseUrl' : 'https://api.themoviedb.org/3' //BASE URL FOR MOVIEDB API
+
+};

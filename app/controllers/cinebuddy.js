977dc5b0c02f4bd08560e4bea9700dcca7dd9c65
benwingz <benjamin.roullet@gmail.com>
2017-01-26 16:24:32 +0100

    :bug: Handle already signed in user

--- app/controllers/cinebuddy.js
+++ app/controllers/cinebuddy.js
@@ -3,22 +3,42 @@ var request = require('request');
 var mongoose = require('mongoose');
 
 var User = require('../models/user');
+var Showing = require('../models/showing');
 
 exports.createUser = function(req, res) {
   if(!req.body.name || !req.body.password || !req.body.email) {
     res.json({ success: false, message: "Missing parameters, user can't be created."});
   } else {
-    var newUser = new User({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
+    User.findOne({req.body.email}, function(err, user) {
+      if (err) throw err;
+      if (user) {
+        res.json({ success: false, message: 'This email is already used'});
+      } else {
+        var newUser = new User({
+          name: req.body.name,
+          password: req.body.password,
+          email: req.body.email,
+        })
 
-    newUser.save(function(err) {
-      if(err) throw err;
+        newUser.save(function(err) {
+          if(err) throw err;
 
-      console.log('User created');
-      res.json({ success: true, message: 'User have been successfully created'});
-    })
+          console.log('User created');
+          res.json({ success: true, message: 'User have been successfully created'});
+        });
+      }
+    });
   }
 };
+
+/*exports.createShowing = function(req, res) {
+  console.log(req.body);
+  var newShowing = new Showing(req.body);
+
+  newShowing.save(function(err) {
+    if (err) throw err;
+
+    console.log('New Showing created');
+    res.json({ success: true, message: 'Showing have been successfully created'})
+  });
+};*/

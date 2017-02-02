4297d727f2aad8117531d5e60097230b77694eaa
benwingz <benjamin.roullet@gmail.com>
2017-02-02 13:16:03 +0100

    :hammer: Create user controller manager

--- app/controllers/cinebuddy.js
+++ app/controllers/cinebuddy.js
@@ -1,50 +1,6 @@
-var config = require('../../config');
-var request = require('request');
 var mongoose = require('mongoose');
 
-var User = require('../models/user');
 var Showing = require('../models/showing');
-
-exports.createUser = function(req, res) {
-  if(!req.body.name || !req.body.password || !req.body.email) {
-    res.json({ success: false, message: "Missing parameters, user can't be created."});
-  } else {
-    User.findOne({email: req.body.email}, function(err, user) {
-      if (err) throw err;
-      if (user) {
-        res.json({ success: false, message: 'This email is already used'});
-      } else {
-        var newUser = new User({
-          name: req.body.name,
-          password: req.body.password,
-          email: req.body.email,
-        })
-
-        newUser.save(function(err) {
-          if(err) throw err;
-
-          console.log('User created');
-          res.json({ success: true, message: 'User have been successfully created'});
-        });
-      }
-    });
-  }
-};
-
-exports.findUser = function(req, res) {
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-  if(token) {
-    User.findOne({token: token}, function(err, user) {
-      if (err) throw err;
-      if (!user) {
-        res.json({ success: false, message: 'No user relative to this token'});
-      } else {
-        res.json(user);
-      }
-    });
-  }
-};
 
 exports.createShowing = function(req, res) {
   if (!req.body.movieid || !req.body.userid || !req.body.availability) {
